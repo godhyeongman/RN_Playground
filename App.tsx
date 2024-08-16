08406db937280973c7aac4386c1ed4f5a0981777
dott <aaaa4000@naver.com>
2024-08-16 12:32:52 +0900

    rn cli 환경 개발

--- App.tsx
+++ App.tsx
@@ -5,9 +5,12 @@
  * @format
  */
 
-import React from 'react';
+import {atom, useAtom, useAtomValue, useSetAtom} from 'jotai';
+
+import React, {Suspense} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -29,31 +32,12 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const fakeFetch = async (num: number) => {
+  return num;
+};
+
+const numAtom = atom(1);
+const dataAtom = atom(async get => await fakeFetch(get(numAtom)));
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -63,39 +47,31 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <View>
+        <Text>테스트</Text>
+        <Suspense fallback={<Text>로딩중</Text>}>
+          <Test></Test>
+        </Suspense>
+      </View>
     </SafeAreaView>
   );
 }
 
+const Test = () => {
+  const [test, setTest] = useAtom(numAtom);
+  const data = useAtomValue(dataAtom);
+  return (
+    <>
+      <Button
+        title="클릭"
+        onPress={() => {
+          setTest(prev => prev + 1);
+        }}></Button>
+      <Text>{data}</Text>
+    </>
+  );
+};
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
